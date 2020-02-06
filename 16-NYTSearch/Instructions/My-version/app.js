621af80b6f79d14c8b3d64565bf30e05857a6c1a
Casey Carroll <caseydean8@gmail.com>
2020-02-06 18:03:32 -0500

    Sends query with user search term

--- 16-NYTSearch/Instructions/My-version/app.js
+++ 16-NYTSearch/Instructions/My-version/app.js
@@ -1,13 +1,25 @@
-console.log("hello")
+console.log("hello");
 
 const apiKey = "BkLkeT4lQuDfhKNLm9yIbUKXgjkL5auf";
+// let search = '';
 
 // /articlesearch.json?q={query}&fq={filter}
-const queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${apiKey}`
+// const queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&api-key=${apiKey}`;
 
-$.ajax({
-  url: queryUrl,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-})
+const apiGet = (search) => {
+  const queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&api-key=${apiKey}`;
+  console.log(queryUrl);
+
+  $.ajax({
+    url: queryUrl,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  })
+};
+
+$("button").on("click", function(event) {
+  event.preventDefault();
+  let search = $("#search-term").val();
+  apiGet(search);
+});
