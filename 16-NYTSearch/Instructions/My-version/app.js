2a7ac3e385be34a9439ab07792b755308660ad54
Casey Carroll <caseydean8@gmail.com>
2020-02-06 11:38:23 -0500

    App.js retrieves data from NYT api

--- 16-NYTSearch/Instructions/My-version/app.js
+++ 16-NYTSearch/Instructions/My-version/app.js
@@ -1,1 +1,13 @@
 console.log("hello")
+
+const apiKey = "BkLkeT4lQuDfhKNLm9yIbUKXgjkL5auf";
+
+// /articlesearch.json?q={query}&fq={filter}
+const queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${apiKey}`
+
+$.ajax({
+  url: queryUrl,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+})
