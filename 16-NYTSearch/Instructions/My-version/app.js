3842f0595e7411a634af00657147e842c49d06f3
Casey Carroll <caseydean8@gmail.com>
2020-02-07 09:53:57 -0500

    Added correct syntax to query for date range

--- 16-NYTSearch/Instructions/My-version/app.js
+++ 16-NYTSearch/Instructions/My-version/app.js
@@ -5,9 +5,11 @@ const apiKey = "BkLkeT4lQuDfhKNLm9yIbUKXgjkL5auf";
 
 // /articlesearch.json?q={query}&fq={filter}
 // const queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&api-key=${apiKey}`;
+let startDate;
+let endDate;
 
 const apiGet = (search) => {
-  const queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&api-key=${apiKey}`;
+  const queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&facet_fields=source&facet=true&begin_date=20160101&end_date=20160131&api-key=${apiKey}`;
   console.log(queryUrl);
 
   $.ajax({
