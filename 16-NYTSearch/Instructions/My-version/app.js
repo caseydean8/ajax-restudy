74d71e37c2465db73dcc59ccd429daaceb712e1a
Casey Carroll <caseydean8@gmail.com>
2020-02-07 18:26:36 -0500

    API data appended to markdown

--- 16-NYTSearch/Instructions/My-version/app.js
+++ 16-NYTSearch/Instructions/My-version/app.js
@@ -1,42 +1,37 @@
-console.log("hello");
 
-const apiKey = "BkLkeT4lQuDfhKNLm9yIbUKXgjkL5auf";
-// let search = '';
-
-// /articlesearch.json?q={query}&fq={filter}
-// const queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}&api-key=${apiKey}`;
-// let startDate;
-// let endDate;
-
-const apiGet = (search, startDate, endDate) => {
-  (startDate) ? startDate = `&begin_date=${startDate}` : startDate = '';
-  (endDate) ? endDate = `&end_date=${endDate}` : endDate = '';
+const apiGet = (search, resultNum, startDate, endDate) => {
+  startDate ? (startDate = `&begin_date=${startDate}`) : (startDate = "");
+  endDate ? (endDate = `&end_date=${endDate}`) : (endDate = "");
+  const apiKey = "BkLkeT4lQuDfhKNLm9yIbUKXgjkL5auf";
   const queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?&q=${search}&facet_fields=source&facet=true${startDate}${endDate}&fl=web_url&api-key=${apiKey}`;
-  console.log(queryUrl);
 
   $.ajax({
     url: queryUrl,
     method: "GET"
   }).then(function(res) {
-    console.log(res);
-    console.log(res.response.docs);
-    const result = [];
-    // let docu = res.reponse.docs[0].web_url;
-  })
+    const result = res.response.docs;
+    articleDisplay(result, resultNum);
+  });
 };
 
 $("button").on("click", function(event) {
   event.preventDefault();
+  $("#div3").empty();
   let search = $("#search-term").val();
-  let startDate = $("#start").val().replace(/-/g, "");
-  let endDate = $("#end").val().replace(/-/g, "")
-  console.log(startDate, endDate);
-  apiGet(search, startDate, endDate);
+  let startDate = $("#start")
+    .val()
+    .replace(/-/g, "");
+  let endDate = $("#end")
+    .val()
+    .replace(/-/g, "");
+  let resultNum = $("#records").val();
+  apiGet(search, resultNum, startDate, endDate);
 });
 
-const articleDisplay = () => {
-  artArr = [];
-  for (let doc of docs) {
-
-  }
-}
+const articleDisplay = (results, resultNum) => {
+  results.forEach(result => {
+    const p = $("<p>");
+    $(p).text(result.web_url);
+    $(".div3").append(p);
+  });
+};
