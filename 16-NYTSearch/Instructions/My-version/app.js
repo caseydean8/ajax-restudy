a2933b3a42b7ea26b346399509c76753f8f4e3e6
Casey Carroll <caseydean8@gmail.com>
2020-02-07 19:56:27 -0500

    Displays number of results under 10

--- 16-NYTSearch/Instructions/My-version/app.js
+++ 16-NYTSearch/Instructions/My-version/app.js
@@ -1,10 +1,9 @@
-
+// let resultNum = 0;
 const apiGet = (search, resultNum, startDate, endDate) => {
   startDate ? (startDate = `&begin_date=${startDate}`) : (startDate = "");
   endDate ? (endDate = `&end_date=${endDate}`) : (endDate = "");
   const apiKey = "BkLkeT4lQuDfhKNLm9yIbUKXgjkL5auf";
   const queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?&q=${search}&facet_fields=source&facet=true${startDate}${endDate}&fl=web_url&api-key=${apiKey}`;
-
   $.ajax({
     url: queryUrl,
     method: "GET"
@@ -16,7 +15,6 @@ const apiGet = (search, resultNum, startDate, endDate) => {
 
 $("button").on("click", function(event) {
   event.preventDefault();
-  $("#div3").empty();
   let search = $("#search-term").val();
   let startDate = $("#start")
     .val()
@@ -24,14 +22,17 @@ $("button").on("click", function(event) {
   let endDate = $("#end")
     .val()
     .replace(/-/g, "");
-  let resultNum = $("#records").val();
+  resultNum = $("#records").val();
+  console.log(resultNum);
   apiGet(search, resultNum, startDate, endDate);
 });
 
 const articleDisplay = (results, resultNum) => {
+  $(".div2").empty();
+  if (resultNum)  results.length = resultNum;
   results.forEach(result => {
     const p = $("<p>");
     $(p).text(result.web_url);
-    $(".div3").append(p);
+    $(".div2").append(p);
   });
 };
